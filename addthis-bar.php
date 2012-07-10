11d3a6787094780791162122e14b3cca194f46b3
jorbin <jorbin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-10 17:32:51 +0000

    add blank php
    git-svn-id: https://plugins.svn.wordpress.org/addthis-welcome/trunk@570186 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ addthis-bar.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: AddThis Welcome Bar
+Plugin URI: http://www.addthis.com
+Description: The Welcome Bar from AddThis
+Version: 1.0
+Author: AddThis
+Author URI: http://www.addthis.com
+License: GPL
+*/
